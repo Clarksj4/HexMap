24adbc9f5c5f9fa0b2ae3e7605c1bda8fc9ec395
Stephen <clarksj4@student.op.ac.nz>
2017-08-06 18:38:34 +1200

    Move area serialization hack to area script

--- Assets/Scripts/Area.cs
+++ Assets/Scripts/Area.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 
+[Serializable]
 public class Area
 {
     public List<KeyValuePair<int, Shape>> Shapes { get; private set; }
+
+    //
+    // TODO: serialize / deserialize to area. This stuff is here as a hacky way to serialize shapes
+    //
+    public Line[] Lines;
+    public int[] LineIndices;
+
+    public Ring[] Rings;
+    public int[] RingIndices;
 
     public Area()
     {
@@ -31,6 +41,16 @@ public class Area
 
     public IEnumerable<AxialCoordinate> From(AxialCoordinate origin, HexRotation orientation)
     {
+        ConstructArea();
         return Shapes.Select(kvp => kvp.Value).SelectMany(s => s.From(origin, orientation));
+    }
+
+    private void ConstructArea()
+    {
+        for (int i = 0; i < Lines.Length; i++)
+            Add(LineIndices[i], Lines[i]);
+
+        for (int i = 0; i < Rings.Length; i++)
+            Add(RingIndices[i], Rings[i]);
     }
 }
--- Assets/Scripts/Quake.cs
+++ Assets/Scripts/Quake.cs
@@ -10,47 +10,17 @@ public class Quake : ScriptableObject
     public float Duration;
     public float PropogationDelay = 0.1f;
     public QuakeCell QuakeCellPrefab;
-
-    //
-    // TODO: serialize / deserialize to area. This stuff is here as a hacky way to serialize shapes
-    //
-    public Line[] Lines;
-    public int[] LineIndices;
-
-    public Ring[] Rings;
-    public int[] RingIndices;
-
-    // TODO: serialize / deserialize this
-    private Area area = new Area();
+    public Area Area;
 
     public void DoAt(AxialCoordinate origin, HexRotation orientation)
     {
-        ConstructArea();
-
         HexMap map = FindObjectOfType<HexMap>();
         map.StartCoroutine(DoQuake(origin, orientation));
-
-        //IEnumerable<AxialCoordinate> coords = area.From(origin, orientation).OrderBy(c => c.Distance(origin));
-        //int tiers = coords.Last().Distance(origin) + 1;
-
-        //HexMap map = FindObjectOfType<HexMap>();
-        //foreach (var coord in coords)
-        //{
-        //    HexCell cell = map[coord];
-        //    if (cell)
-        //    {
-        //        int tier = coord.Distance(origin);
-        //        float t = tier / (float)tiers;
-        //        float delay = tier * PropogationDelay;
-
-        //        QuakeCellPrefab.DoAt(cell, Intensity * (1 - t), Duration - delay, delay);
-        //    }
-        //}
     }
 
     IEnumerator DoQuake(AxialCoordinate origin, HexRotation orientation)
     {
-        IEnumerable<AxialCoordinate> coords = area.From(origin, orientation).OrderBy(c => c.Distance(origin));
+        IEnumerable<AxialCoordinate> coords = Area.From(origin, orientation).OrderBy(c => c.Distance(origin));
         int tiers = coords.Last().Distance(origin) + 1;
         int previousTier = 0;
 
@@ -72,14 +42,5 @@ public class Quake : ScriptableObject
                 QuakeCellPrefab.DoAt(cell, Intensity * (1 - t), Duration - delay);
             }
         }
-    }
-
-    private void ConstructArea()
-    {
-        for (int i = 0; i < Lines.Length; i++)
-            area.Add(LineIndices[i], Lines[i]);
-
-        for (int i = 0; i < Rings.Length; i++)
-            area.Add(RingIndices[i], Rings[i]);
     }
 }
